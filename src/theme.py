e5e01b99e52cabc86aa659c541b68d36c70cbfbe
Ian Lantzy <ilantzy@udel.edu>
2016-01-20 19:34:21 -0500

    Added theme python class

---
+++ src/theme.py
@@ -0,0 +1,14 @@
+"""peyl theme module.
+"""
+
+import json
+
+class Theme(object):
+    """peyl Theme class.
+    """
+    def __init__(self, path=""):
+        self.json_path = path + '/style.json'
+        json_data = json.load(open(self.json_path))
+        self.name = json_data["name"]
+        self.author = json_data["author"]
+        self.css_path = self.name + '/style.css'
